4c37bd40f319985993078cd01c2ceafe5700783b
jeanhebert <jeanheberth19@gmail.com>
2024-03-07 11:14:45 -0300

    criado a função do botão entrar do login.js

--- src/views/login.js
+++ src/views/login.js
@@ -4,6 +4,16 @@ import FormGroup from "./components/form-group";
 
 
 class Login extends React.Component {
+
+    state = {
+        email: '',
+        senha: ''
+    }
+    entrar = () => {
+        console.log('Email', this.state.email)
+        console.log('Senha', this.state.senha)
+    }
+
     render() {
         return (
             <div className={"container"}>
@@ -15,21 +25,28 @@ class Login extends React.Component {
                                     <div className={'col-lg-12'}>
                                         <div className={'bs-component'}>
                                             <fieldset>
-                                                    <FormGroup label={'Email: *'} htmlFor={'exampleInputEmail1'}>
+                                                <FormGroup label={'Email: *'} htmlFor={'exampleInputEmail1'}>
                                                     <input type={'email'}
+                                                           value={this.state.email}
+                                                           onChange={event => this.setState({email: event.target.value})}
                                                            className={'form-control'}
                                                            id={'exampleInputEmail1'}
                                                            aria-describedby={'emamilHelp'}
                                                            placeholder={'Digite o email'}/>
 
-                                                    </FormGroup>
+                                                </FormGroup>
                                                 <FormGroup label={'Senha: *'} htmlFor={'exampleInputPassword1'}>
                                                     <input type="password"
+                                                           value={this.state.senha}
+                                                           onChange={event => this.setState({senha: event.target.value})}
                                                            className="form-control"
                                                            id="exampleInputPassword1"
                                                            placeholder="Password"/>
 
                                                 </FormGroup>
+                                                <button onClick={this.entrar} className="btn btn-success">Entrar
+                                                </button>
+                                                <button className="btn btn-danger">Cadastrar</button>
                                             </fieldset>
                                         </div>
                                     </div>
